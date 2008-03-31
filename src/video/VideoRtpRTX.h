f8c2f1e0f8988945c8f660c7285aaf7442e2a34a
Alexis S. Bourrelle <bourrelle@polymtl.ca>
2008-03-30 21:02:25 -0400

    Video thread now close when hanging up.

--- src/video/VideoRtpRTX.h
+++ src/video/VideoRtpRTX.h
@@ -32,7 +32,7 @@
 #include "VideoRtp.h"
 #include <cc++/thread.h>
 #include <ccrtp/rtp.h>
-#include <ffmpeg/avcodec.h>
+#include <ffmpeg/avcodec.h> //TODO: p-e pas nescessaire !
 #include "V4L/VideoDeviceManager.h"
 #include "../mixer/VideoInput.h"
 #include "../mixer/VideoOutput.h"
@@ -79,6 +79,8 @@ private:
     /** SYMMETRIC RTP Session to send/receive */
     ost::SymmetricRTPSession* session;
 
+    bool _sym;
+
     /** Codec for encoding */
     VideoCodec* 	encodeCodec;
     /** Codec for decoding */
@@ -95,11 +97,11 @@ private:
 
     int codecClockRate; // sample rate of the codec we use to encode and decode (most of time 90000HZ)
 
-    /** buffer for received Data */
-   char* receiveDataDecoded;
+    unsigned char *data_to_send;
+    unsigned char *data_from_peer;
+    unsigned char *data_from_wc;
+    unsigned char *data_to_display;
 
-    /** Buffer for Data to send */
-    unsigned char* sendDataEncoded;
 
 	/**
 	 * Get the data from V4l, send it to the mixer, encode and send to RTP
