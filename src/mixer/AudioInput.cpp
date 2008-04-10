0badc8a4fb11f2b7407bce2deef46641ffd8736b
Jean Tessier <jean.tessier@polymtl.ca>
2008-04-10 15:43:28 -0400

    Added splitter fucntions to audio inputs

--- src/mixer/AudioInput.cpp
+++ src/mixer/AudioInput.cpp
@@ -21,6 +21,7 @@
 
 AudioInput::AudioInput(){
   sem_init(&sem_putData,0,1);
+  this->fifo.push_back( std::list<AudioPacket*>() );
 }
 
 AudioInput::~AudioInput(){
@@ -30,19 +31,20 @@ void AudioInput::putData(short *data, int size, int timeStamp)
 {
   if (data!=NULL && size > 0)
   {
-    
-    // Creating an audio data packet 
-    AudioPacket * tmpPak= new AudioPacket;
-    tmpPak->data= new short[size];
-    tmpPak->size= size;
-    tmpPak->timeStamp= new TimeInfo(timeStamp); 
-    
-    memcpy(tmpPak->data, data, tmpPak->size);
-
-	// Adding data to the fifo
-	sem_wait(&sem_putData); 
-    	this->fifo.push_back(tmpPak);
-    sem_post(&sem_putData);
+    for( int i= 0; i < this->fifo.size(); i++ ){
+	    // Creating an audio data packet 
+	    AudioPacket * tmpPak= new AudioPacket;
+	    tmpPak->data= new short[size];
+	    tmpPak->size= size;
+	    tmpPak->timeStamp= new TimeInfo(timeStamp); 
+	    
+	    memcpy(tmpPak->data, data, tmpPak->size);
+	
+		// Adding data to the fifo
+		sem_wait(&sem_putData); 
+	    	this->fifo[i].push_back(tmpPak);
+	    sem_post(&sem_putData);
+    }
    
   }
   else{
@@ -50,15 +52,15 @@ void AudioInput::putData(short *data, int size, int timeStamp)
   }   
 }
 
-short* AudioInput::fetchData(int &size) 
+short* AudioInput::fetchData(int &size, int index) 
 { 
   short* data;
   
-  if(this->fifo.size() != 0)
+  if(this->fifo[index].size() != 0)
   {
   	// Getting reference to head in queue
   	sem_wait(&sem_putData);
-    AudioPacket * tmpPak= this->fifo.front();
+    AudioPacket * tmpPak= this->fifo[index].front();
     sem_post(&sem_putData);
         
     data = tmpPak->data;
@@ -66,7 +68,7 @@ short* AudioInput::fetchData(int &size)
     
     // Removing head
     sem_wait(&sem_putData); 
-    this->fifo.pop_front();
+    this->fifo[index].pop_front();
     sem_post(&sem_putData);
     
     return data;
@@ -84,29 +86,41 @@ short* AudioInput::fetchData(int &size)
   return NULL;
 }
 
-int AudioInput::getSizeBuffer(){
+int AudioInput::getSizeBuffer(int index){
 		
-	if( this->fifo.size() == 0)
+	if( this->fifo[index].size() == 0)
 		return 0;
 	
 	sem_wait(&sem_putData);
-	AudioPacket * tmpPak= this->fifo.front();
+	AudioPacket * tmpPak= this->fifo[index].front();
 	int sizeBuffer= tmpPak->size;
 	sem_post(&sem_putData);
 	return sizeBuffer;
 	
 }
 
-TimeInfo* AudioInput::fetchTimeInfo(){
+TimeInfo* AudioInput::fetchTimeInfo(int index){
  
-    if( this->fifo.size() == 0)
+    if( this->fifo[index].size() == 0)
 		return 0;
 	
 	sem_wait(&sem_putData);
-	AudioPacket * tmpPak= this->fifo.front();
+	AudioPacket * tmpPak= this->fifo[index].front();
 	TimeInfo* tmpInfo= tmpPak->timeStamp;
 	sem_post(&sem_putData);
 	return tmpInfo;
 	
 }
 
+int AudioInput::AddOutput(){
+	this->fifo.push_back( std::list<AudioPacket*>() );
+	return this->fifo.size() - 1;
+}
+    
+void AudioInput::RemoveOutput( int index ){
+	
+	if( index > this->fifo.size() ){
+		std::vector< std::list<AudioPacket*> >::iterator pos= this->fifo.begin();	
+		this->fifo.erase( pos + index );
+	}		
+}
