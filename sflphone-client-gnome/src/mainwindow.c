5f5d028fa5769bf90cce9978cd353fb0e367bb30
pierre-luc <pierre-luc@aqra.ca>
2009-06-26 10:57:05 -0400

    [#1732] The ACCOUNT_ZRTP_NOT_SUPP_WARNING is now taken into account for ip2ip calls.

--- sflphone-client-gnome/src/mainwindow.c
+++ sflphone-client-gnome/src/mainwindow.c
@@ -355,6 +355,10 @@ main_window_zrtp_not_supported(callable_obj_t * c)
         DEBUG("Warning Enabled %s", warning_enabled);
     } else {
         DEBUG("Account is null callID %s", c->_callID);
+            GHashTable * properties = NULL;
+            properties = sflphone_get_ip2ip_properties();
+            if(properties != NULL)
+              { warning_enabled = g_hash_table_lookup(properties, ACCOUNT_ZRTP_NOT_SUPP_WARNING); }
     }
     
     if(g_strcasecmp(warning_enabled,"TRUE") == 0) {
