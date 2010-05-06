6c301351555137b16b8f45bbf1c45695447e8658
Pierre-Luc Bacon <pierrelucbacon@aqra.ca>
2010-05-06 09:06:40 -0400

    Shared memory segment wrapper.

---
+++ sflphone-common/src/util/ipc/SharedMemoryPosix.cpp
@@ -0,0 +1,156 @@
+/*
+ *  Copyright (C) 2006-2010 Savoir-Faire Linux inc.
+ *  Author: Pierre-Luc Bacon <pierre-luc.bacon@savoirfairelinux.com>
+ *                                                                              
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *                                                                                
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *                                                                              
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+ 
+#include "SharedMemoryPosix.h"
+
+#include <iostream>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <assert.h>
+
+namespace sfl
+{
+
+SharedMemoryPosix::SharedMemoryPosix(const std::string& name, bool exclusive, std::ios_base::openmode mode) throw(SharedMemoryException):
+	fd(0),
+	size(0),
+	mappedAddr(NULL),
+	name(name),
+	exclusive(exclusive),
+	mode(mode)
+{
+	int oflags = getOflags(mode);
+	if (exclusive) {
+		oflags |= O_CREAT | O_EXCL;
+	} else {
+		oflags |= O_CREAT;
+	}
+	
+	if ((fd = shm_open(name.c_str(), oflags, 0)) < 0) {
+		throw SharedMemoryException(std::string("shm_open(): ") + strerror(errno));
+	}
+	
+	this->truncate();
+	this->map();
+}
+
+SharedMemoryPosix::~SharedMemoryPosix()
+{
+	unmap();
+	this->close();
+	this->unlink();
+}
+
+void SharedMemoryPosix::truncate()
+{
+	int pageSize = sysconf(_SC_PAGE_SIZE);
+	truncate (pageSize);
+}
+
+void SharedMemoryPosix::truncate(off_t size)
+{
+	assert(fd);
+	
+	if (ftruncate(fd, size) < 0) {
+		throw SharedMemoryException(std::string("Truncate: ") + strerror(errno));
+	}
+	
+	this->size = size;
+}
+
+void SharedMemoryPosix::map() throw(SharedMemoryException)
+{
+	assert(fd);
+	
+	int prot = getProtFlags(mode);
+	
+	mappedAddr = mmap(NULL, (size_t) size, prot, MAP_SHARED, fd, 0);
+	if (mappedAddr == MAP_FAILED) {
+		this->close();
+		this->unlink();		
+		throw SharedMemoryException(std::string("mmap(): ") + strerror(errno));
+	}
+}
+
+void SharedMemoryPosix::unmap() throw(SharedMemoryException)
+{
+	assert(mappedAddr);
+	
+	if (munmap(mappedAddr, size) < 0) {
+		throw SharedMemoryException(std::string("mumap(): ") + strerror(errno));
+	}
+	
+	mappedAddr = NULL;
+	size = 0;
+}
+
+void SharedMemoryPosix::close() throw(SharedMemoryException)
+{
+	assert(fd);
+
+	if (::close(fd) < 0) {
+		throw SharedMemoryException(std::string("close(): ") + strerror(errno));
+	}
+	
+	fd = 0;
+}
+
+void SharedMemoryPosix::unlink() throw(SharedMemoryException)
+{
+	if (shm_unlink(name.c_str()) < 0) {
+		throw SharedMemoryException(std::string("unlink(): ") + strerror(errno));
+	}
+}
+
+int SharedMemoryPosix::getOflags(std::ios_base::openmode mode)
+{
+	int oflags = 0;
+	if ((mode & std::ios::in) && (mode & std::ios::out)) {
+		oflags |= O_RDWR;
+	} else if (mode & std::ios::in){
+		oflags |= O_RDONLY;
+	}
+	
+	if (mode & std::ios::trunc) {
+		oflags |= O_TRUNC;
+	}
+	
+	return oflags;
+}
+
+int SharedMemoryPosix::getProtFlags(std::ios_base::openmode mode)
+{
+	int prot = PROT_NONE;
+	
+	if (mode & std::ios::in) {
+		prot = PROT_READ | PROT_EXEC;
+	}
+	
+	if (mode & std::ios::out) {
+		prot |= PROT_WRITE | PROT_EXEC;
+	}
+	
+	return prot;
+}
+
+}
---
+++ sflphone-common/src/util/ipc/SharedMemoryPosix.h
@@ -0,0 +1,121 @@
+/*
+ *  Copyright (C) 2006-2010 Savoir-Faire Linux inc.
+ *  Author: Pierre-Luc Bacon <pierre-luc.bacon@savoirfairelinux.com>
+ *                                                                              
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *                                                                                
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *                                                                              
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#ifndef SHAREDMEMORYPOSIX_H_
+#define SHAREDMEMORYPOSIX_H_
+
+#include <stdexcept>
+#include <iostream>
+
+namespace sfl
+{
+
+	/**
+	 * This exception is thrown upon various errors that might occur during 
+	 * operations on a shared memory segment.
+	 */
+	class SharedMemoryException : public std::runtime_error
+	{
+		public:
+			SharedMemoryException(std::string const& msg) : std::runtime_error(msg) {}
+	};
+
+	/**
+	 * This class is a wrapper class
+	 * around posix shared memory functions (shm_open). 
+	 * It aims at being a quick way to set up 
+	 * a shared memory object.
+	 */
+	class SharedMemoryPosix 
+	{
+		public: 
+			/**
+			 * Create a new shared memory segment, and mmap it to to the process address space. 
+			 * @param name Path name to the shared memory region. It need not be a path to an existing file. 
+			 * @param mode Access mode for the memory region. Valid modes are in | out, in, and their combination with trunc.
+			 * "app", "ate" and "b" don't have any effect and will be ignored. 
+			 * @param exclusive If set to true, if the shared memory exists, the constructor will fail. 
+			 */
+			SharedMemoryPosix(const std::string& name, bool exclusive, std::ios_base::openmode mode = std::ios_base::in | std::ios_base::out) throw(SharedMemoryException);
+			
+			/**
+			 * Shared memory destructor. It will close, unmap, and unlink the shared memory object.
+			 */
+			virtual ~SharedMemoryPosix();
+			
+			/**
+			 * This causes the shared memory object to have size of length bytes.
+			 * @param length The new size for the shared memory object.
+			 */
+			void truncate(off_t length);
+			
+			/**
+			 * Truncate the shared memory object to one unit of the page size. 
+			 * @sa truncate(off_t length)
+			 */
+			void truncate();
+			
+			/**
+			 * @return a pointer to the shared memory region. 
+			 */
+			void * getRegion() { return mappedAddr; }
+			
+		protected:
+			/**
+			 * Close the shared memory through its file descriptor.
+			 */
+			void close() throw(SharedMemoryException);
+			
+			/**
+			 * Remove the name of the shared memory object.
+			 */
+			void unlink() throw(SharedMemoryException);
+			
+			/**
+			 * Establish a mapping between a process' address space and a file and the shared memory object.
+			 */
+			void map() throw(SharedMemoryException);
+			
+			/**
+			 * Removes any mappings for those entire pages containing any part of the address space of the process.
+			 */
+			void unmap() throw(SharedMemoryException);
+			
+			/**
+			 * @return The file status flags and file access modes of the open file description. 
+			 */
+			int getOflags(std::ios_base::openmode mode);
+			
+			/**
+			 * @return Read, write, execute, or some combination of accesses are permitted to the data being mapped.
+			 */
+			int getProtFlags(std::ios_base::openmode mode);
+			
+		private:
+			int fd;
+			off_t size;
+			bool exclusive;
+			void * mappedAddr;
+			
+			std::string name;
+			std::ios_base::openmode mode;
+	};
+
+}
+
+#endif /*SHAREDMEMORYPOSIX_H_*/
